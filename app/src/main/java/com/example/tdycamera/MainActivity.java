70eb93442770f45999af84d0429494b91ef4bb08
tangdanyu <15698135927@163.com>
2021-08-03 13:15:06 +0800

    调用相机应用录制视频

--- app/src/main/java/com/example/tdycamera/MainActivity.java
+++ app/src/main/java/com/example/tdycamera/MainActivity.java
@@ -1,13 +1,6 @@
 package com.example.tdycamera;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.FileProvider;
-
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -17,8 +10,12 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
-import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -27,17 +24,20 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, ActivityCompat.OnRequestPermissionsResultCallback {
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener, ActivityCompat.OnRequestPermissionsResultCallback  {
     private String TAG = getClass().getSimpleName();
-    private static final int PERMISSION_REQUEST_CAMERA = 0;
     private static final int REQ_1 = 1;
     private static final int REQ_2 = 2;
-    private View mLayout;
+    private static final int REQ_3 = 3;
     private ImageView ivThumbnail;
     private ImageView ivComplete;
+    private VideoView vvVideo;
     private Button btnTakePicture1;
     private Button btnTakePicture2;
-    private String currentPhotoPath;
+    private Button btnTakeVideo;
+    private String currentPhotoPath;//拍摄照片路径
+    private String currentVideoPath;//录像存储路径
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,13 +45,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         initView();
     }
     private void initView(){
-        mLayout = findViewById(R.id.main_layout);
         btnTakePicture1 = findViewById(R.id.btn_takePicture1);
         btnTakePicture2 = findViewById(R.id.btn_takePicture2);
+        btnTakeVideo = findViewById(R.id.btn_takeVideo);
         ivThumbnail = findViewById(R.id.iv_thumbnail);
         ivComplete = findViewById(R.id.iv_complete);
+        vvVideo = findViewById(R.id.vv_video);
         btnTakePicture1.setOnClickListener(this);
         btnTakePicture2.setOnClickListener(this);
+        btnTakeVideo.setOnClickListener(this);
 
     }
 
@@ -63,6 +65,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.btn_takePicture2:
                 takePictureGetFile();
+                break;
+            case R.id.btn_takeVideo:
+                takeVideo();
                 break;
         }
     }
@@ -93,6 +98,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
     }
+
+    //调用相机应用录制视频
+    private void takeVideo() {
+        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {//如果是null,应用会崩溃
+            File videoFile = null;
+            try {
+                videoFile = createVideoFile();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            if (videoFile != null) {
+                Uri videoURI = FileProvider.getUriForFile(this,
+                        this.getPackageName()+".fileprovider",
+                        videoFile);
+                takeVideoIntent.putExtra(MediaStore.EXTRA_OUTPUT, videoURI);
+                startActivityForResult(takeVideoIntent, REQ_3);
+            }
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -121,18 +147,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             }
         }
+
+        //观看视频
+        if (requestCode == REQ_3 && resultCode == RESULT_OK) {
+            Uri videoUri = data.getData();
+            vvVideo.setMediaController(new MediaController(this));
+            vvVideo.setVideoURI(videoUri);
+//            vvVideo.setVideoPath(currentVideoPath);
+            vvVideo.start();
+        }
     }
 
     private File createImageFile() throws IOException {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);//私密
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);//需要在file_paths.xml中配置Pictures
         File image = File.createTempFile(
                 imageFileName,  /* 前缀 */
                 ".jpg",         /* 后缀 */
                 storageDir      /* 目录 */
         );
         currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+    private File createVideoFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "Video_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_MOVIES);//需要在file_paths.xml中配置Movies
+        File image = File.createTempFile(
+                imageFileName,  /* 前缀 */
+                ".mp4",         /* 后缀 */
+                storageDir      /* 目录 */
+        );
+        currentVideoPath = image.getAbsolutePath();
         return image;
     }
     //对调整后的图片进行解码
@@ -157,63 +204,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
         ivComplete.setImageBitmap(bitmap);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        //动态申请权限
-        if (requestCode == PERMISSION_REQUEST_CAMERA) {
-            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Snackbar.make(mLayout, R.string.camera_permission_granted,
-                        Snackbar.LENGTH_SHORT)
-                        .show();
-                startCamera();
-            } else {
-                Snackbar.make(mLayout, R.string.camera_permission_denied,
-                        Snackbar.LENGTH_SHORT)
-                        .show();
-            }
-        }
-    }
-    private void showCameraPreview() {
-        // 检查Camera权限是否已经获得
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-                == PackageManager.PERMISSION_GRANTED) {
-            Snackbar.make(mLayout,
-                    R.string.camera_permission_available,
-                    Snackbar.LENGTH_SHORT).show();
-            startCamera();
-        } else {
-            requestCameraPermission();
-        }
-
-    }
-
-    //请求相机权限
-    private void requestCameraPermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                Manifest.permission.CAMERA)) {
-            Snackbar.make(mLayout, R.string.camera_access_required,
-                    Snackbar.LENGTH_INDEFINITE).setAction(R.string.ok, new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    ActivityCompat.requestPermissions(MainActivity.this,
-                            new String[]{Manifest.permission.CAMERA},
-                            PERMISSION_REQUEST_CAMERA);
-                }
-            }).show();
-
-        } else {
-            Snackbar.make(mLayout, R.string.camera_unavailable, Snackbar.LENGTH_SHORT).show();
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
-        }
-    }
-
-    private void startCamera() {
-        Intent intent = new Intent(this, CameraPreviewActivity.class);
-        startActivity(intent);
     }
 
 }
